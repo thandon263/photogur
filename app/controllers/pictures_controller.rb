6cd6e95b319cf7cad915a8c6814b98e2dc5cabf7
thandon263 <ncubethando20@gmail.com>
2016-06-15 10:25:44 -0400

    The functionality for delete on the photogur

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -25,11 +25,6 @@ class PicturesController < ApplicationController
 
   end
 
-  private
-  def picture_params
-    params.require(:picture).permit(:artist, :title, :url)
-  end
-
   def edit
       @picture = Picture.find(params[:id])
   end
@@ -38,9 +33,22 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
 
     if @picture.update_attributes(picture_params)
-      redirect_to "/pictures/#{picture.id}"
+      redirect_to "/pictures/#{@picture.id}"
     else
       render :edit
     end
   end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
 end
